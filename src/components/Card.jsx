09268a7f9400edaf084ca76022128f0052495762
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-06 15:07:51 +0100

    basic styles for Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,25 @@
-export function Card() {
+export function Card({country}) {
+
+    const {
+        flags,
+        name,
+        population,
+        region,
+        capital
+    } = country
+
     return (
-        <div>Card</div>
+        <article className="bg-white flex flex-col justify-center shrink-0 w-80 my-8 border border-solid border-gray-200 rounded shadow hover:shadow-xl hover:cursor-pointer transition-shadow duration-200">
+            <img className="h-40 rounded drop-shadow-xl"
+                src={flags.svg}
+                alt="{name}"
+            />
+            <div className="p-4 mb-8">
+                <h2 className="my-4 font-bold text-xl">{name.official}</h2>
+                <p className="mb-1"><span className="font-bold">Population:</span> {population}</p>
+                <p className="mb-1"><span className="font-bold">Region:</span> {region}</p>
+                <p><span className="font-bold">Capital:</span> {capital}</p>
+            </div>
+        </article>
     )
 }
