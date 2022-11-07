6bd11da5ffc6fd432b2749b26dba1ca6504ca5c3
kkulek <106318909+kkulek@users.noreply.github.com>
2022-11-07 10:36:50 +0100

    added svg arrow (file & tailwind content property)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      content: {
+        arrow: 'url("./img/left-arrow.svg")'
+      }
+    },
   },
   plugins: [],
 }
